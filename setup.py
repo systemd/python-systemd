6edaadeb084687b7fe4afe1ec886e3b588f0b3e7
David Strauss <david@davidstrauss.net>
2012-06-25 23:15:28 -0700

    Add systemd-journal library.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup, Extension
 
 journald = Extension('journald',
-                    sources = ['journald.c'])
+                     libraries = ['systemd-journal'],
+                     sources = ['journald.c'])
 
 setup (name = 'journald',
        version = '0.1',
        description = 'Native interface to the journald facilities of systemd',
+       author_email = 'david@davidstrauss.net',
+       url = 'https://github.com/davidstrauss/journald-python',
        ext_modules = [journald])
