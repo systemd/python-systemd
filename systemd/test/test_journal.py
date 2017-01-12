5bc6dea50bd58dcbd513a057ec3e004e8fd96d45
Jeongsoo Park <toracle@gmail.com>
2017-01-12 04:00:34 -0800

    replace dict.iteritems() with dict.items() to support py3
    py3 doesn't have dict.iteritems() anymore.

--- systemd/test/test_journal.py
+++ systemd/test/test_journal.py
@@ -5,12 +5,39 @@ import logging
 import os
 import time
 import uuid
+import traceback as _traceback
 
 from systemd import journal, id128
+from systemd.journal import _make_line
 
 import pytest
 
 TEST_MID = uuid.UUID('8441372f8dca4ca98694a6091fd8519f')
+
+class MockSender:
+    def __init__(self):
+        self.buf = []
+
+    def send(self, MESSAGE, MESSAGE_ID=None,
+             CODE_FILE=None, CODE_LINE=None, CODE_FUNC=None,
+             **kwargs):
+        args = ['MESSAGE=' + MESSAGE]
+
+        if MESSAGE_ID is not None:
+            id = getattr(MESSAGE_ID, 'hex', MESSAGE_ID)
+            args.append('MESSAGE_ID=' + id)
+
+        if CODE_LINE == CODE_FILE == CODE_FUNC == None:
+            CODE_FILE, CODE_LINE, CODE_FUNC = _traceback.extract_stack(limit=2)[0][:3]
+        if CODE_FILE is not None:
+            args.append('CODE_FILE=' + CODE_FILE)
+        if CODE_LINE is not None:
+            args.append('CODE_LINE={:d}'.format(CODE_LINE))
+        if CODE_FUNC is not None:
+            args.append('CODE_FUNC=' + CODE_FUNC)
+
+        args.extend(_make_line(key, val) for key, val in kwargs.items())
+        self.buf.append(args)
 
 @contextlib.contextmanager
 def skip_enosys():
@@ -57,6 +84,15 @@ def test_journalhandler_init_exception():
 def test_journalhandler_init():
     kw = {'X':3, 'X3':4}
     journal.JournalHandler(logging.INFO, **kw)
+
+def test_journalhandler_info():
+    record = logging.LogRecord('test-logger', logging.INFO, 'testpath', 1, 'test', None, None)
+
+    sender = MockSender()
+    kw = {'X':3, 'X3':4, 'SENDER_FUNCTION': sender.send}
+    handler = journal.JournalHandler(logging.INFO, **kw)
+    handler.emit(record)
+    assert len(sender.buf) == 1
 
 def test_reader_init_flags():
     j1 = journal.Reader()
